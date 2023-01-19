c185f6d0d764556aca13634af85bc8c9a9d48b8a
kmihiranga <kalanamihiranga9@hotmail.com>
2023-01-19 09:57:39 +0530

    implemented post service

---
+++ src/main/java/com/springboot/blog/service/impl/PostServiceImple.java
@@ -0,0 +1,14 @@
+package com.springboot.blog.service.impl;
+
+import com.springboot.blog.payload.PostDto;
+import com.springboot.blog.service.PostService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PostServiceImple implements PostService {
+
+    @Override
+    public PostDto createPost(PostDto postDto) {
+        return null;
+    }
+}
